56f3f2123204f78f2871acf09c431891e55fd63d
SantiagoWL1 <rgnhr43rtg@outlook.com>
2024-09-06 18:57:24 +0400

    Form1.Designer.cs

---
+++ Aviator Predictor/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Aviator_Hack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            APUsername = new TextBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            Aviator = new LinkLabel();
+            ForgotPassword = new LinkLabel();
+            label4 = new Label();
+            APPassword = new TextBox();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Red;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(659, 31);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(3, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Aviator";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Dock = DockStyle.Right;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(622, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(37, 31);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderColor = Color.White;
+            button2.FlatAppearance.MouseOverBackColor = Color.Lime;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(89, 202);
+            button2.Name = "button2";
+            button2.Size = new Size(96, 31);
+            button2.TabIndex = 1;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // APUsername
+            // 
+            APUsername.Location = new Point(45, 82);
+            APUsername.Name = "APUsername";
+            APUsername.Size = new Size(231, 23);
+            APUsername.TabIndex = 2;
+            APUsername.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(89, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Aviator Predictor";
+            label1.Click += label1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(Aviator);
+            panel2.Controls.Add(ForgotPassword);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(APPassword);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(APUsername);
+            panel2.Controls.Add(button2);
+            panel2.Location = new Point(179, 110);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(304, 300);
+            panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(17, 131);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 25);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(17, 82);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(25, 25);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Aviator
+            // 
+            Aviator.AccessibleDescription = "";
+            Aviator.AccessibleName = "";
+            Aviator.ActiveLinkColor = Color.Red;
+            Aviator.AutoSize = true;
+            Aviator.LinkColor = Color.WhiteSmoke;
+            Aviator.Location = new Point(118, 248);
+            Aviator.Name = "Aviator";
+            Aviator.Size = new Size(45, 15);
+            Aviator.TabIndex = 8;
+            Aviator.TabStop = true;
+            Aviator.Text = "Aviator";
+            Aviator.LinkClicked += Aviator_LinkClicked;
+            // 
+            // ForgotPassword
+            // 
+            ForgotPassword.ActiveLinkColor = Color.Red;
+            ForgotPassword.AutoSize = true;
+            ForgotPassword.LinkColor = Color.WhiteSmoke;
+            ForgotPassword.Location = new Point(181, 159);
+            ForgotPassword.Name = "ForgotPassword";
+            ForgotPassword.Size = new Size(95, 15);
+            ForgotPassword.TabIndex = 7;
+            ForgotPassword.TabStop = true;
+            ForgotPassword.Text = "Forgot Password";
+            ForgotPassword.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(45, 115);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Password";
+            // 
+            // APPassword
+            // 
+            APPassword.Location = new Point(45, 133);
+            APPassword.Name = "APPassword";
+            APPassword.PasswordChar = '*';
+            APPassword.Size = new Size(231, 23);
+            APPassword.TabIndex = 5;
+            APPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(45, 64);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Username";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Dock = DockStyle.Fill;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(658, 526);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(658, 526);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox3);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Aviator Predictor";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Label label2;
+        private Button button2;
+        private TextBox APUsername;
+        private Label label1;
+        private Panel panel2;
+        private Label label3;
+        private LinkLabel ForgotPassword;
+        private Label label4;
+        private TextBox APPassword;
+        private LinkLabel Aviator;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+    }
+}
